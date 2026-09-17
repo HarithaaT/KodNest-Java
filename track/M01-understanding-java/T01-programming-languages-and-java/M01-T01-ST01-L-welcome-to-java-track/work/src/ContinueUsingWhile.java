d628085f46409f1500027bae0d51b44605a0802f
HarithaaT <harithaa1124@gmail.com>
2026-09-17 16:49:31 +0530

    ContinueUsingWhile

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ContinueUsingWhile.java
@@ -0,0 +1,15 @@
+
+public class ContinueUsingWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
