c278f9c21aabde7861fa3029ea070ea884685488
HarithaaT <harithaa1124@gmail.com>
2026-09-17 16:59:16 +0530

    HomeWork3

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/HomeWork/ContinueUsingDoWhile.java
@@ -0,0 +1,15 @@
+
+public class ContinueUsingDoWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
