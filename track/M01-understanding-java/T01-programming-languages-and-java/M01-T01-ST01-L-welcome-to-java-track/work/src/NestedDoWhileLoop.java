dd26ff1f570b2794e26866679b6b99faed609616
HarithaaT <harithaa1124@gmail.com>
2026-09-17 15:44:28 +0530

    NestedDoWhileLoop

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/NestedDoWhileLoop.java
@@ -0,0 +1,15 @@
+
+public class NestedDoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.println(i + " " + j);
+                j++;
+            } while (j <= 2);
+            i++;
+        } while (i <= 3);
+    }
+}
